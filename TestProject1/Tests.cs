85b94220f36bf1a52e0dd12e40ac45f438e4ce46
zdl8061 <381605825@qq.com>
2020-05-13 12:53:56 +0800

    test

---
+++ TestProject1/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var i = 1;
+            var t = 3 / i;
+        }
+
+        [Theory]
+        [InlineData(2,3)]
+        [InlineData(4,0)]
+        [InlineData(2,9)]
+        public void Test2(int i,int t)
+        {
+            var result = i / t;
+        }
+    }
+}
